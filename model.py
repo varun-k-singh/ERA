0922ec1ccf362ac0657dfe7452bc98c6a06c242d
varun-k-singh <varun_k_singh@outlook.com>
2023-06-08 12:33:24 +0530

    code modularity

---
+++ model.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torchsummary import summary
+from tqdm import tqdm
+from utils import GetCorrectPredCount
+
+class Net(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3,bias=False) #bias I have added to network as per the question requirement.
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3,bias=False)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3,bias=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3,bias=False)
+        self.fc1 = nn.Linear(4096, 50,bias=False)
+        self.fc2 = nn.Linear(50, 10,bias=False)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2)) 
+        x = F.relu(self.conv3(x), 2)
+        x = F.relu(F.max_pool2d(self.conv4(x), 2)) 
+        x = x.view(-1,4096)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def model_summary(model, input_size):
+    # use_cuda = torch.cuda.is_available()
+    # device = torch.device("cuda" if use_cuda else "cpu")
+    summary(model, input_size)
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+def model_train(model, device, train_loader, optimizer):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = F.nll_loss(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def model_test(model, device, test_loader):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
---
+++ utils.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def return_dataset_images(train_loader, total_images):
+    batch_data, batch_label = next(iter(train_loader)) 
+
+    fig = plt.figure()
+
+    for i in range(total_images):
+        plt.subplot(3,4,i+1)
+        plt.tight_layout()
+        plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+        plt.title(batch_label[i].item())
+        plt.xticks([])
+        plt.yticks([])
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()    
